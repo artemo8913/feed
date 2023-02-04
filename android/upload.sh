879e86ea61f6d6c01d079aa7a5b6cca3d1811099
Alexey <a@cherepanov.info>
2023-02-04 15:33:00 +0300

    fix token

--- android/upload.sh
+++ android/upload.sh
@@ -1,4 +1,2 @@
-echo ${YA_DISK_TOKEN}
-
 /opt/ydcmd/ydcmd.py --token=${YA_DISK_TOKEN} put /upload/app-debug.apk /ins/
 /opt/ydcmd/ydcmd.py --token=${YA_DISK_TOKEN} put /upload/app-release-unsigned.apk /ins/
