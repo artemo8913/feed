0251fb630221754b89fbc1ba84a12467c2cdf1fa
Alexey <a@cherepanov.info>
2023-02-04 15:14:14 +0300

    disable cache

--- android/upload.sh
+++ android/upload.sh
@@ -1,2 +1,4 @@
+echo ${YA_DISK_TOKEN}
+
 /opt/ydcmd/ydcmd.py --token=${YA_DISK_TOKEN} put /upload/app-debug.apk /ins/
 /opt/ydcmd/ydcmd.py --token=${YA_DISK_TOKEN} put /upload/app-release-unsigned.apk /ins/
