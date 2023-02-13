db86d5e7fcc6228ee783e0093b8e5f320ce87e8e
Alexey <a@cherepanov.info>
2023-02-13 10:34:58 +0300

    fix port

--- entry.sh
+++ entry.sh
@@ -3,7 +3,7 @@
 cd /app/packages/api
 yarn run db:seed
 ls -1al /app/packages/api/_db
-PORT=4000 /app/node_modules/.bin/nest start &
+PORT=4262 /app/node_modules/.bin/nest start &
 
 cd /app/packages/admin
 PORT=4301 /app/node_modules/.bin/next start &
