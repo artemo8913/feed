dcba53a5d7a10c966b3d7417fda62f3a86bde910
Alexey <a@cherepanov.info>
2023-02-20 15:48:01 +0300

    use legacy api

--- packages/scanner/src/config.tsx
+++ packages/scanner/src/config.tsx
@@ -1,3 +1,3 @@
 const DEV = process.env.NODE_ENV !== 'production';
 
-export const API_DOMAIN = DEV ? 'http://localhost:5000' : `https://${window.location.host}`;
+export const API_DOMAIN = DEV ? 'http://localhost:5000' : `https://yclins.cherepusick.keenetic.name`;
