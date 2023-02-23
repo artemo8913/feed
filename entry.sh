39453445eeb6d43a9f57c8f0ebee8813e5cfe79d
Alexey <a@cherepanov.info>
2023-02-23 12:52:38 +0300

    run seeds

--- entry.sh
+++ entry.sh
@@ -2,7 +2,9 @@
 
 cd /app/packages/api
 #yarn run db:seed
-#ls -1al /app/packages/api/_db
+ls -1al /app/packages/api/_db
+rm -rf /app/packages/api/_db/*
+/app/node_modules/.bin/nest yarn run db:seed
 PORT=4262 /app/node_modules/.bin/nest start &
 
 cd /app/packages/admin
