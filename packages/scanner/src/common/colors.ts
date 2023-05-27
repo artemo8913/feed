7952a0d3178244962d7ef91226ad8c025d27a4b0
volnyagin <vconst@users.noreply.github.com>
2023-05-27 11:43:01 +0300

    fix lint

--- packages/scanner/src/common/colors.ts
+++ packages/scanner/src/common/colors.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
 export type Colors = {
-    'red': '#ff0000';
-    'green': '#00ff00';
-    'yellow': '#fad900';
-    'blue': '#182bff';
-    'dark': '#333333';
-    'light': '#f5f5f5';
+    red: '#ff0000';
+    green: '#00ff00';
+    yellow: '#fad900';
+    blue: '#182bff';
+    dark: '#333333';
+    light: '#f5f5f5';
 };
