50f5cd9809fca0c7c75e93bc298173e1182f2b37
volnyagin <vconst@users.noreply.github.com>
2023-05-31 01:10:12 +0300

    fix basePath for admin and api path for scanner

--- packages/scanner/src/config.tsx
+++ packages/scanner/src/config.tsx
@@ -1,4 +1,5 @@
 const DEV = process.env.NODE_ENV !== 'production';
+const NEW_API_URL_ENV = process.env.NEW_API_URL_ENV;
 
-export const API_DOMAIN = DEV ? 'http://localhost:4000/api/v1' : `https://feed.cherepusick.keenetic.name/api/v1`;
+export const API_DOMAIN = NEW_API_URL_ENV || (DEV ? 'http://localhost:4000/api/v1' : `https://feed.cherepusick.keenetic.name/api/v1`);
 // export const API_DOMAIN = `https://yclins.cherepusick.keenetic.name`;
