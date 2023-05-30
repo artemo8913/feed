e8a82fc131b3c0329e735002b60614fb6981e630
Arthur <stfriman13@gmail.com>
2023-05-30 13:24:55 +0300

    stats layout rework

--- packages/scanner/src/components/stats/stats-date-picker/stats-date-picker.tsx
+++ packages/scanner/src/components/stats/stats-date-picker/stats-date-picker.tsx
@@ -7,12 +7,13 @@ import { ReactComponent as Next } from '~/icons/arrow_right_small.svg';
 import style from './stats-date-picker.module.css';
 
 interface StatsDatePickerProps {
+    progress: boolean;
     statsDate: StatsDateEnum;
     formattedDate: string;
     setStatsDate: (StatsDateEnum) => void;
 }
 
-export const StatsDatePicker = ({ formattedDate, setStatsDate, statsDate }: StatsDatePickerProps) => {
+export const StatsDatePicker = ({ formattedDate, progress, setStatsDate, statsDate }: StatsDatePickerProps) => {
     const prevClickHandle = () => {
         if (statsDate === StatsDateEnum.today) {
             setStatsDate(StatsDateEnum.yesterday);
@@ -33,11 +34,19 @@ export const StatsDatePicker = ({ formattedDate, setStatsDate, statsDate }: Stat
 
     return (
         <div className={style.datePicker}>
-            <button className={style.button} disabled={statsDate === StatsDateEnum.yesterday} onClick={prevClickHandle}>
+            <button
+                className={style.button}
+                disabled={statsDate === StatsDateEnum.yesterday || progress}
+                onClick={prevClickHandle}
+            >
                 <Prev className={style.svg} />
             </button>
             <span className={style.date}>{formattedDate}</span>
-            <button className={style.button} disabled={statsDate === StatsDateEnum.tomorrow} onClick={nextClickHandle}>
+            <button
+                className={style.button}
+                disabled={statsDate === StatsDateEnum.tomorrow || progress}
+                onClick={nextClickHandle}
+            >
                 <Next className={style.svg} />
             </button>
         </div>
