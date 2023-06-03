06b9a55e440ac8ae598e1a187d81740e820dc9ad
volnyagin <vconst@users.noreply.github.com>
2023-06-03 16:13:46 +0300

    fix kitchen in history

--- packages/admin/src/components/entities/feed-transaction/list.tsx
+++ packages/admin/src/components/entities/feed-transaction/list.tsx
@@ -42,7 +42,7 @@ export const FeedTransactionList: FC<IResourceComponentsProps> = () => {
             }),
             {}
         );
-    }, [vols]);
+    }, [kitchens]);
 
     return (
         <List>
