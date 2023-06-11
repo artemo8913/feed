adea0153f862cf8d810eb65dad9c5fdccbdf619f
volnyagin <vconst@users.noreply.github.com>
2023-06-11 18:05:52 +0300

    try use proddev to debug nest problem

--- entry.sh
+++ entry.sh
@@ -6,7 +6,9 @@ rm -rf /app/packages/api/_db/*
 PORT=4262 /app/node_modules/.bin/nest start &
 
 cd /app/packages/admin
-PORT=4301 /app/node_modules/.bin/next start &
+#PORT=4301 /app/node_modules/.bin/next start &
+
+yarn proddev &
 
 cd /app/backend
 ./manage.py migrate
